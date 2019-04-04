aafabf6ff7ae5cbf0a22efbd7fc0c14812e2125c
mrWeiss <giorgio2197@gmail.com>
2019-04-04 14:31:21 +0200

    add project entity

---
+++ sources/design/project_reti_logiche.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity project_reti_logiche is
+    port(
+        i_clk     : in  std_logic;
+        i_start   : in  std_logic;
+        i_rst     : in  std_logic;
+        i_data    : in  std_logic_vector(7 downto 0);
+        o_address : out std_logic_vector(15 downto 0);
+        o_done    : out std_logic;
+        o_en      : out std_logic;
+        o_we      : out std_logic;
+        o_data    : out std_logic_vector(7 downto 0)
+    );
+end project_reti_logiche;
+
+architecture structural of project_reti_logiche is
+    begin
+end structural;
